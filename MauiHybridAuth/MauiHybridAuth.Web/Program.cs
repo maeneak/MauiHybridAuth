dea7572d61950e551c00f6da1744ae05fd683aed
Beth Massi <bethma@microsoft.com>
2024-12-19 13:08:37 -0800

    Code refactor / cleanup

--- MauiHybridAuth/MauiHybridAuth.Web/Program.cs
+++ MauiHybridAuth/MauiHybridAuth.Web/Program.cs
@@ -59,17 +59,17 @@ var app = builder.Build();
 app.MapIdentityApi<ApplicationUser>();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+    app.UseSwagger();
+    app.UseSwaggerUI();    
+}
+else
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
-}
-else
-{
-    app.UseMigrationsEndPoint();
-    app.UseSwagger();
-    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
--- MauiHybridAuth/MauiHybridAuth/Services/MauiAuthenticationStateProvider.cs
+++ MauiHybridAuth/MauiHybridAuth/Services/MauiAuthenticationStateProvider.cs
@@ -1,9 +1,33 @@
-﻿using MauiHybridAuth.Shared.Services;
+using Microsoft.AspNetCore.Components.Authorization;
+using System.Threading.Tasks;
+using MauiHybridAuth.Models;
+using System.Security.Claims;
+using System.Diagnostics;
+using System.Net.Http.Json;
 
 namespace MauiHybridAuth.Services
 {
-    public class MauiAuthenticationStateProvider : CustomAuthenticationStateProvider, ICustomAuthenticationStateProvider
+    public enum LoginStatus
     {
+        None,
+        Success,
+        Failed
+    }
+    public interface ICustomAuthenticationStateProvider
+    {
+        public LoginStatus LoginStatus { get; set; }
+        Task<AuthenticationState> GetAuthenticationStateAsync();
+        Task LogInAsync(LoginModel loginModel);
+        void Logout();
+    }
+    public class MauiAuthenticationStateProvider : AuthenticationStateProvider, ICustomAuthenticationStateProvider
+    {
+        //TODO: Place this in AppSettings or Client config file
+        protected string LoginUri { get; set; } = "https://localhost:7157/login";
+
+        public LoginStatus LoginStatus { get; set; } = LoginStatus.None;
+        protected ClaimsPrincipal currentUser = new ClaimsPrincipal(new ClaimsIdentity());
+
         public MauiAuthenticationStateProvider()
         {
             //See: https://learn.microsoft.com/dotnet/maui/data-cloud/local-web-services
@@ -12,8 +36,7 @@ namespace MauiHybridAuth.Services
                 DeviceInfo.Platform == DevicePlatform.Android ? LoginUri.Replace("localhost", "10.0.2.2") : LoginUri;
         }
 
-        //Override the HttpClient creation to allow for custom handling of localhost for Android and iOS
-        protected override HttpClient GetHttpClient()
+        private HttpClient GetHttpClient()
         {
 #if WINDOWS || MACCATALYST
             return new HttpClient();
@@ -21,7 +44,67 @@ namespace MauiHybridAuth.Services
             return new HttpClient(new HttpsClientHandlerService().GetPlatformMessageHandler()); 
 #endif
         }
+
+        public override Task<AuthenticationState> GetAuthenticationStateAsync() =>
+            Task.FromResult(new AuthenticationState(currentUser));
+
+        public Task LogInAsync(LoginModel loginModel)
+        {
+            var loginTask = LogInAsyncCore(loginModel);
+            NotifyAuthenticationStateChanged(loginTask);
+
+            return loginTask;
+
+            async Task<AuthenticationState> LogInAsyncCore(LoginModel loginModel)
+            {
+                var user = await LoginWithProviderAsync(loginModel);
+                currentUser = user;
+
+                return new AuthenticationState(currentUser);
+            }
+        }
+        public void Logout()
+        {
+            LoginStatus = LoginStatus.None;
+            currentUser = new ClaimsPrincipal(new ClaimsIdentity());
+            NotifyAuthenticationStateChanged(Task.FromResult(new AuthenticationState(currentUser)));
+        }
+                  
+        private async Task<ClaimsPrincipal> LoginWithProviderAsync(LoginModel loginModel)
+        {
+            ClaimsPrincipal authenticatedUser;
+            LoginStatus = LoginStatus.None;
+
+            try
+            {
+                var httpClient = GetHttpClient();
+                var loginData = new { loginModel.Email, loginModel.Password };
+                var response = await httpClient.PostAsJsonAsync(LoginUri, loginData);
+
+                LoginStatus = response.IsSuccessStatusCode ? LoginStatus.Success : LoginStatus.Failed;
+
+                if (LoginStatus == LoginStatus.Success)
+                {
+                    //var token = response.Content.ReadAsStringAsync().Result;
+                    var claims = new[] { new Claim(ClaimTypes.Name, loginModel.Email) };
+                    var identity = new ClaimsIdentity(claims, "Custom authentication");
+
+                    authenticatedUser = new ClaimsPrincipal(identity);
+                }
+                else
+                    authenticatedUser = new ClaimsPrincipal(new ClaimsIdentity());
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"Error logging in: {ex.ToString()}");
+                authenticatedUser = new ClaimsPrincipal(new ClaimsIdentity());
+            }
+
+            return authenticatedUser;
+        }
+
     }
+
     public class HttpsClientHandlerService
     {
         public HttpMessageHandler GetPlatformMessageHandler()
@@ -42,7 +125,7 @@ namespace MauiHybridAuth.Services
         };
         return handler;
 #else
-     throw new PlatformNotSupportedException("Only Android and iOS supported.");
+            throw new PlatformNotSupportedException("Only Android and iOS supported.");
 #endif
         }
 
@@ -53,6 +136,5 @@ namespace MauiHybridAuth.Services
     }
 #endif
     }
+
 }
-
-
