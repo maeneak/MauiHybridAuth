bac5f7dc49a0658250fdd1065bde174cc7cc0236
maeneak <20371477+maeneak@users.noreply.github.com>
2025-02-25 00:26:47 +1000

    Profile Menu

--- MauiHybridAuth/MauiHybridAuth.Shared/Theme/CustomAppTheme.cs
+++ MauiHybridAuth/MauiHybridAuth.Shared/Theme/CustomAppTheme.cs
@@ -1,14 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MauiHybridAuth.Shared.Theme
 {
-    public class CustomAppTheme : MudBlazor.MudTheme
+    public class CustomAppTheme : MudBlazor.MudTheme, INotifyPropertyChanged
     {
-        public bool DarkMode { get; set; }
-        public bool SystemDarkMode { get; set; }
+        ThemeColorMode _currentMode = ThemeColorMode.System;
+        bool _systemInDarkMode;
+        public bool InDarkMode { 
+            get {
+                return CurrentMode == ThemeColorMode.System ? SystemInDarkMode : CurrentMode == ThemeColorMode.Dark;
+            }
+            set
+            {
+                _currentMode = value ? ThemeColorMode.Dark : ThemeColorMode.Light;
+            }
+        }
+        public bool SystemInDarkMode
+        {
+            get { return _systemInDarkMode; }
+            set
+            {
+                _systemInDarkMode = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(InDarkMode)));
+            }
+        }
+        public ThemeColorMode CurrentMode {
+            get { return _currentMode; }
+            set {
+                _currentMode = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(InDarkMode)));
+            } 
+        }
+        public event PropertyChangedEventHandler? PropertyChanged;
+    }
+    public enum ThemeColorMode
+    {
+        Light,
+        Dark,
+        System = -1
     }
 }
