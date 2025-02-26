c68aee720bf8e42fcec3791411edf3775327052f
maeneak <20371477+maeneak@users.noreply.github.com>
2025-02-26 16:31:58 +1000

    Profile and profile menu + personal data

---
+++ MauiHybridAuth/MauiHybridAuth.Web/Data/ApplicationRole.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace MauiHybridAuth.Web.Data
+{
+    public class ApplicationRole : IdentityRole
+    {
+        public ApplicationRole() { }
+
+        public ApplicationRole(string roleName)
+            : base(roleName) { }
+
+        public ICollection<IdentityRoleClaim<string>> Claims { get; set; }
+    }
+}
--- MauiHybridAuth/MauiHybridAuth.Web/Data/ApplicationUser.cs
+++ MauiHybridAuth/MauiHybridAuth.Web/Data/ApplicationUser.cs
@@ -5,6 +5,19 @@ namespace MauiHybridAuth.Web.Data
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
+        [PersonalData]
+        public string FirstName { get; set; } = string.Empty;
+        [PersonalData]
+        public string LastName { get; set; } = string.Empty;
+        public byte[] ProfilePicture { get; set; } = new byte[0];
+        public string Initials { 
+            get{
+                return $"{FirstName[0]}{LastName[0]}";
+            }
+        } 
+
+        public ICollection<IdentityUserRole<string>> Roles { get; set; }
+        public ICollection<IdentityUserClaim<string>> Claims { get; set; }
     }
 
 }
