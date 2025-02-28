f8b75e9d22e7aea6cdb987b4cd9e3a806c362ad9
Beth Massi <bethma@microsoft.com>
2025-02-27 17:50:24 -0800

    Add a WeatherService that the Weather page uses and expose an endpoint on the server.

--- MauiHybridAuth/MauiHybridAuth.Web/Program.cs
+++ MauiHybridAuth/MauiHybridAuth.Web/Program.cs
@@ -17,6 +17,7 @@ builder.Services.AddRazorComponents()
 
 // Add device-specific services used by the MauiHybridAuth.Shared project
 builder.Services.AddSingleton<IFormFactor, FormFactor>();
+builder.Services.AddScoped<IWeatherService, WeatherService>();
 
 // Add Auth services used by the Web app
 builder.Services.AddCascadingAuthenticationState();
@@ -89,5 +90,12 @@ app.MapRazorComponents<App>()
     .AddAdditionalAssemblies(typeof(MauiHybridAuth.Shared._Imports).Assembly);
 
 app.MapAdditionalIdentityEndpoints();
+
+//Add the weather API endpoint and require authorization
+app.MapGet("/api/weather", async (IWeatherService weatherService) =>
+{
+    var forecasts = await weatherService.GetWeatherForecastsAsync();
+    return Results.Ok(forecasts);
+}).RequireAuthorization();
 
 app.Run();
